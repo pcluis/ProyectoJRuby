56b523c95de09e8712dc8d3cdbe5dfad634f801e
hohenhain <pcluis12098@gmail.com>
2015-09-28 05:01:44 -0500

    Se agrego UsuarioDTO

---
+++ MercadoSpring/src/main/java/uni/proyecto/dto/UsuarioDTO.java
@@ -0,0 +1,51 @@
+package uni.proyecto.dto;
+
+public class UsuarioDTO {
+
+	private Integer id;
+	private String usuario, clave, nombrecompleto, correo, token;
+	private byte[] foto;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public String getNombrecompleto() {
+		return nombrecompleto;
+	}
+	public void setNombrecompleto(String nombrecompleto) {
+		this.nombrecompleto = nombrecompleto;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public byte[] getFoto() {
+		return foto;
+	}
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+}
