25deffef2f0047c655bf0ec518219b1c7a38969a
hohenhain <pcluis12098@gmail.com>
2015-10-03 09:08:02 -0500

    Se modifico el controlador Login.

--- MercadoSpring/src/main/java/uni/proyecto/controller/LoginController.java
+++ MercadoSpring/src/main/java/uni/proyecto/controller/LoginController.java
@@ -1,16 +1,24 @@
 package uni.proyecto.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import uni.proyecto.dto.PersonaDTO;
+import uni.proyecto.dto.SocioDTO;
 import uni.proyecto.dto.UsuarioDTO;
+import uni.proyecto.util.ListasEstaticas.ESTADOCIVIL;
+import uni.proyecto.util.ListasEstaticas.GENERO;
+import uni.proyecto.util.ListasEstaticas.TIPODOCUMENTO;
 
 @Controller
 @RequestMapping("/")
@@ -22,7 +30,8 @@ public class LoginController extends WebMvcConfigurerAdapter {
 		if(bindingresult.hasErrors()){
 			return new ModelAndView("login.html", "usuarioDTO", usuario);
 		}
-		return new ModelAndView("registrarficha.html", "usuarioDTO", usuario);
+		SocioDTO socioDTO = new SocioDTO();
+		return new ModelAndView("registrarficha.html", "socioDTO", socioDTO);
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "result")
@@ -34,6 +43,39 @@ public class LoginController extends WebMvcConfigurerAdapter {
 		}
 		
 		return "home";
+	}
+	
+	@ModelAttribute("tipoDocumento")
+	public Map<Integer, String> listarTipoDocumento(){
+		
+		Map<Integer, String> mapaDocumento = new HashMap<Integer, String>();
+		for(TIPODOCUMENTO t : TIPODOCUMENTO.values()){
+			mapaDocumento.put(t.getValue(), t.toString());
+		}
+		
+		return mapaDocumento;
+	}
+	
+	@ModelAttribute("genero")
+	public Map<Integer, String> listarGenero(){
+		
+		Map<Integer, String> mapaGenero = new HashMap<Integer, String>();
+		for(GENERO g : GENERO.values()){
+			mapaGenero.put(g.getValue(), g.toString());
+		}
+		
+		return mapaGenero;
+	}
+	
+	@ModelAttribute("estadoCivil")
+	public Map<Integer, String> listarEstadoCivil(){
+		
+		Map<Integer, String> mapaEstadoCivil = new HashMap<Integer, String>();
+		for(ESTADOCIVIL e : ESTADOCIVIL.values()){
+			mapaEstadoCivil.put(e.getValue(), e.toString());
+		}
+		
+		return mapaEstadoCivil;
 	}
 	
 }
