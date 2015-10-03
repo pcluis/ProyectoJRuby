719e7beee6cf367e3532b005c782f60696fc9181
hohenhaim <pcluis12098@gmail.com>
2015-10-02 19:17:04 -0500

    Mejorando las clases ruby gracias a Yisus.

--- MercadoSpring/src/main/resources/ruby/database.rb
+++ MercadoSpring/src/main/resources/ruby/database.rb
@@ -6,7 +6,7 @@ ActiveRecord::Base.establish_connection(
   :host => "localhost",
   :database => 'mercadospring',
   :username => 'root',
-  :password => 'mysql'
+  :password => '123456'
 )
 
 ActiveRecord::Schema.define do
@@ -88,6 +88,59 @@ end
 #
 #episode_names = show.episodes.pluck(:name)
 ##
+
+class Establecimiento < ActiveRecord::Base
+  
+end
+
+
+Establecimiento.create!(tipo: 'P', numero: '1', pabellon: 'A', giro: nil, estado: 0, precio: 1500, mapakey: 'ap1')
+Establecimiento.create!(tipo: 'P', numero: '2', pabellon: 'A', giro: nil, estado: 0, precio: 1500, mapakey: 'ap2')
+Establecimiento.create!(tipo: 'P', numero: '3', pabellon: 'A', giro: nil, estado: 0, precio: 1500, mapakey: 'ap3')
+Establecimiento.create!(tipo: 'P', numero: '4', pabellon: 'A', giro: nil, estado: 0, precio: 1500, mapakey: 'ap4')
+Establecimiento.create!(tipo: 'E', numero: '1', pabellon: 'A', giro: nil, estado: 0, precio: 1000, mapakey: 'ae1')
+Establecimiento.create!(tipo: 'E', numero: '2', pabellon: 'A', giro: nil, estado: 0, precio: 1000, mapakey: 'ae2')
+Establecimiento.create!(tipo: 'E', numero: '3', pabellon: 'A', giro: nil, estado: 0, precio: 1000, mapakey: 'ae3')
+Establecimiento.create!(tipo: 'E', numero: '4', pabellon: 'A', giro: nil, estado: 0, precio: 1000, mapakey: 'ae4')
+Establecimiento.create!(tipo: 'E', numero: '5', pabellon: 'A', giro: nil, estado: 0, precio: 1000, mapakey: 'ae5')
+Establecimiento.create!(tipo: 'E', numero: '6', pabellon: 'A', giro: nil, estado: 0, precio: 1000, mapakey: 'ae6')
+Establecimiento.create!(tipo: 'E', numero: '7', pabellon: 'A', giro: nil, estado: 0, precio: 800, mapakey: 'ae7')
+Establecimiento.create!(tipo: 'E', numero: '8', pabellon: 'A', giro: nil, estado: 0, precio: 900, mapakey: 'ae8')
+Establecimiento.create!(tipo: 'E', numero: '9', pabellon: 'A', giro: nil, estado: 0, precio: 900, mapakey: 'ae9')
+Establecimiento.create!(tipo: 'P', numero: '5', pabellon: 'A', giro: nil, estado: 0, precio: 2000, mapakey: 'ap5')
+Establecimiento.create!(tipo: 'P', numero: '6', pabellon: 'A', giro: nil, estado: 0, precio: 2000, mapakey: 'ap6')
+Establecimiento.create!(tipo: 'P', numero: '7', pabellon: 'A', giro: nil, estado: 0, precio: 2000, mapakey: 'ap7')
+
+Establecimiento.create!(tipo: 'P', numero: '1', pabellon: 'B', giro: nil, estado: 0, precio: 1200, mapakey: 'bp1')
+Establecimiento.create!(tipo: 'P', numero: '2', pabellon: 'B', giro: nil, estado: 0, precio: 1200, mapakey: 'bp2')
+Establecimiento.create!(tipo: 'P', numero: '3', pabellon: 'B', giro: nil, estado: 0, precio: 1200, mapakey: 'bp3')
+Establecimiento.create!(tipo: 'P', numero: '4', pabellon: 'B', giro: nil, estado: 0, precio: 1200, mapakey: 'bp4')
+Establecimiento.create!(tipo: 'J', numero: '1', pabellon: 'B', giro: nil, estado: 0, precio: 400, mapakey: 'bj1')
+Establecimiento.create!(tipo: 'J', numero: '2', pabellon: 'A', giro: nil, estado: 0, precio: 400, mapakey: 'bj2')
+Establecimiento.create!(tipo: 'J', numero: '3', pabellon: 'B', giro: nil, estado: 0, precio: 400, mapakey: 'bj3')
+Establecimiento.create!(tipo: 'J', numero: '4', pabellon: 'B', giro: nil, estado: 0, precio: 400, mapakey: 'bj4')
+Establecimiento.create!(tipo: 'E', numero: '1', pabellon: 'B', giro: nil, estado: 0, precio: 1200, mapakey: 'be1')
+Establecimiento.create!(tipo: 'E', numero: '2', pabellon: 'B', giro: nil, estado: 0, precio: 1100, mapakey: 'be2')
+Establecimiento.create!(tipo: 'E', numero: '3', pabellon: 'B', giro: nil, estado: 0, precio: 1100, mapakey: 'be3')
+Establecimiento.create!(tipo: 'E', numero: '4', pabellon: 'B', giro: nil, estado: 0, precio: 1000, mapakey: 'be4')
+Establecimiento.create!(tipo: 'P', numero: '5', pabellon: 'B', giro: nil, estado: 0, precio: 2000, mapakey: 'bp5')
+Establecimiento.create!(tipo: 'P', numero: '6', pabellon: 'B', giro: nil, estado: 0, precio: 2000, mapakey: 'bp6')
+Establecimiento.create!(tipo: 'P', numero: '7', pabellon: 'B', giro: nil, estado: 0, precio: 2000, mapakey: 'bp7')
+
+Establecimiento.create!(tipo: 'P', numero: '1', pabellon: 'C', giro: nil, estado: 0, precio: 1500, mapakey: 'cp1')
+Establecimiento.create!(tipo: 'P', numero: '2', pabellon: 'C', giro: nil, estado: 0, precio: 1200, mapakey: 'cp2')
+Establecimiento.create!(tipo: 'P', numero: '3', pabellon: 'C', giro: nil, estado: 0, precio: 1500, mapakey: 'cp3')
+Establecimiento.create!(tipo: 'P', numero: '4', pabellon: 'C', giro: nil, estado: 0, precio: 1200, mapakey: 'cp4')
+Establecimiento.create!(tipo: 'J', numero: '1', pabellon: 'C', giro: nil, estado: 0, precio: 600, mapakey: 'cj1')
+Establecimiento.create!(tipo: 'J', numero: '2', pabellon: 'C', giro: nil, estado: 0, precio: 400, mapakey: 'cj2')
+Establecimiento.create!(tipo: 'J', numero: '3', pabellon: 'C', giro: nil, estado: 0, precio: 500, mapakey: 'cj3')
+Establecimiento.create!(tipo: 'J', numero: '4', pabellon: 'C', giro: nil, estado: 0, precio: 800, mapakey: 'cj4')
+Establecimiento.create!(tipo: 'J', numero: '5', pabellon: 'C', giro: nil, estado: 0, precio: 400, mapakey: 'cj5')
+Establecimiento.create!(tipo: 'J', numero: '6', pabellon: 'C', giro: nil, estado: 0, precio: 300, mapakey: 'cj6')
+
+
+
+
 
 class Greeter < ActiveRecord::Base
 
--- MercadoSpring/src/main/resources/ruby/model/socio.rb
+++ MercadoSpring/src/main/resources/ruby/model/socio.rb
@@ -1,14 +1,25 @@
 require 'rubygems'
 require 'activerecord-jdbc-adapter'
 
-class SocioDAO
-
-  class Socio < ActiveRecord::Base
+class Socio < ActiveRecord::Base
   	has_many :documentos
-  end
-
-  def self.create()
-  	Socio.create!(nombres: 'Luis')
-  end
-
+  	
+  	def self.nuevoSocio param = "sin parametro"
+  	  s = Socio.new
+  	  s.nombres = param
+  	  if s.save
+  	  	puts s.nombres 
+  	  else
+  	    puts s.errors.full.messages
+  	  end
+  	end
+  	
+  	def self.leerSocio id = nil
+  	  if id.present?
+  	  	s = Socio.find(id)
+  	  	ActiveRecord::Base.include_root_in_json = true
+  	  	s.to_json
+  	  end
+  	end
+  	
 end
